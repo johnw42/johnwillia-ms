75ab55bf8e86c997f2f55fe5443bfdbc95c36a77
John Williams <williamsjohn@microsoft.com>
2023-05-06 23:37:12 -0700

    using a request body

--- pages/api/ms-graph-login.js
+++ pages/api/ms-graph-login.js
@@ -17,25 +17,24 @@ const handler = async (req, res) => {
   });
 
   const authResponse = await fetch(
-    'https://login.microsoftonline.com/common/oauth2/v2.0/token' +
-      '?client_id=' +
-      req.query.client_id +
-      '&redirect_uri=' +
-      encodeURIComponent(req.query.redirect_uri) +
-      '&grant_type=authorization_code' +
-      '&code=' +
-      encodeURIComponent(req.query.code) +
-      // '&code_verifier=' +
-      // encodeURIComponent(req.query.code_verifier) +
-      '&client_secret=' +
-      encodeURIComponent(process.env.AZURE_CLIENT_SECRET ?? ''),
-    { method: 'post' }
+    'https://login.microsoftonline.com/common/oauth2/v2.0/token',
+    {
+      method: 'post',
+      body:
+        'client_id=' +
+        req.query.client_id +
+        '&redirect_uri=' +
+        encodeURIComponent(req.query.redirect_uri) +
+        '&grant_type=authorization_code' +
+        '&code=' +
+        encodeURIComponent(req.query.code) +
+        '&code_verifier=' +
+        encodeURIComponent(req.query.code_verifier) +
+        '&client_secret=' +
+        encodeURIComponent(process.env.AZURE_CLIENT_SECRET ?? ''),
+    }
   );
-  res.end(
-    res
-      .status(authResponse.status)
-      .send(authResponse.text())
-  );
+  res.end(res.status(authResponse.status).send(authResponse.text()));
 };
 
 export default handler;
