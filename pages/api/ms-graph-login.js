050dea0415ada0a6695f1f3912f96a421f442798
John Williams <williamsjohn@microsoft.com>
2023-05-06 23:02:08 -0700

    added CORS

--- pages/api/ms-graph-login.js
+++ pages/api/ms-graph-login.js
@@ -1,21 +1,34 @@
+const Cors = require('cors');
+
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD'],
+});
+
 // https://nextjs.org/docs/pages/building-your-application/routing/api-routes
-const handler = async (
-  req,
-  res,
-) => {
+const handler = async (req, res) => {
+  await new Promise((resolve, reject) => {
+    cors(req, res, (result) => {
+      if (result instanceof Error) {
+        reject(result);
+        return;
+      }
+      resolve(result);
+    });
+  });
+
   const authResponse = await fetch(
     'https://login.microsoftonline.com/common/oauth2/v2.0/token' +
-    '?client_id=' +
-    req.query.client_id +
-    '&redirect_uri=' +
-    req.query.redirect_uri +
-    '&grant_type=authorization_code' +
-    '&code=' +
-    req.query.code +
-    '&code_verifier=' +
-    req.query.code_verifier +
-    '&client_secret=' +
-    encodeURIComponent(process.env.AZURE_CLIENT_SECRET ?? ''),
+      '?client_id=' +
+      req.query.client_id +
+      '&redirect_uri=' +
+      req.query.redirect_uri +
+      '&grant_type=authorization_code' +
+      '&code=' +
+      req.query.code +
+      '&code_verifier=' +
+      req.query.code_verifier +
+      '&client_secret=' +
+      encodeURIComponent(process.env.AZURE_CLIENT_SECRET ?? ''),
     { method: 'post' }
   );
   res.end(res.status(authResponse.status).send(authResponse.text()));
